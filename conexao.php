8243017080d454ea4e2a37ab833e51ce7c6d97b0
tayna goncalves <taynagon7302@hotmail.com>
2025-05-30 15:09:43 -0300

    a

--- conexao.php
+++ conexao.php
@@ -1,14 +1,16 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "123456";
+$dbname = "sistema_cadastro
 
-$host = 'localhost';
-$dbname = 'sistema_cadastro';
-$user = 'root';
-$pass = '123456';
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro de conexão: " . $e->getMessage());
+$conexao = new mysqli($host, $user, $pass, $dbname);
+
+
+if ($conexao->connect_error) {
+    die("Erro na conexão: " . $conexao->connect_error);
+} else {
+    echo "Conexão realizada com sucesso!";
 }
 ?>
